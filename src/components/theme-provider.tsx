e7e86f181da4166fe0f848102ee7550f7ed38814
vibhamaurya05 <vibhamaurya5sep@gmail.com>
2025-04-17 15:23:38 +0530

    feat: set default theme dark

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -24,7 +24,7 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
 
 export function ThemeProvider({
   children,
-  defaultTheme = "system",
+  defaultTheme = "dark",
   ...props
 }: ThemeProviderProps) {
   const [theme, setTheme] = useState<Theme>(
